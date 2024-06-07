adaa20f56b0367bedd29ae6f2c0408be79e74459
clemenclay <clemenclay@gmail.com>
2024-06-06 23:08:30 -0300

    sdfsdfssssssssssss

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const SURVEY_ID = 1;
-
+<script>
 const json = "{\r\n \"title\": \"Formulario para Declaracion Jurada\",\r\n \"completedHtml\": \"<h3>Thank you for your feedback<\/h3>\",\r\n \"completedHtmlOnCondition\": [\r\n  {\r\n   \"html\": \"<h3>Thank you for your feedback<\/h3> <h4>We are glad that you love our product. Your ideas and suggestions will help us make it even better.<\/h4>\"\r\n  },\r\n  {\r\n   \"html\": \"<h3>Thank you for your feedback<\/h3> <h4>We are glad that you shared your ideas with us. They will help us make our product better.<\/h4>\"\r\n  }\r\n ],\r\n \"pages\": [\r\n  {\r\n   \"name\": \"page1\",\r\n   \"elements\": [\r\n    {\r\n     \"type\": \"text\",\r\n     \"name\": \"question1\",\r\n     \"title\": \"Nombre\",\r\n     \"isRequired\": true\r\n    },\r\n    {\r\n     \"type\": \"checkbox\",\r\n     \"name\": \"question3\",\r\n     \"title\": \"Confirmo Declaracion Jurada\",\r\n     \"isRequired\": true,\r\n     \"choices\": [\r\n      {\r\n       \"value\": \"Item 1\",\r\n       \"text\": \"OK\"\r\n      }\r\n     ]\r\n    },\r\n    {\r\n     \"type\": \"file\",\r\n     \"name\": \"question4\",\r\n     \"title\": \"Suba DNI\",\r\n     \"isRequired\": true\r\n    },\r\n    {\r\n     \"type\": \"signaturepad\",\r\n     \"name\": \"question2\",\r\n     \"title\": \"Firma\",\r\n     \"isRequired\": true\r\n    }\r\n   ]\r\n  }\r\n ],\r\n \"showQuestionNumbers\": \"off\"\r\n}";
 
 function createSurveyPdfModel (surveyModel) {
@@ -55,3 +55,5 @@ survey.data = {
 };
 
 $("#surveyElement").Survey({ model: survey });
+
+</script>
